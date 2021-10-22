341a20db9a3f6e80a26f585b708b7216b65be3de
KCSITDEVELOPMENT <89642513+KCSITDEVELOPMENT@users.noreply.github.com>
2021-10-22 16:11:47 +0200

    filters

--- HansAfriqueApi/Specifications/ProductSpecifications.cs
+++ HansAfriqueApi/Specifications/ProductSpecifications.cs
@@ -9,7 +9,15 @@ namespace HansAfriqueApi.Specifications
 {
     public class ProductSpecifications : BaseSpecification<Part>
     {
-        public ProductSpecifications()
+        public ProductSpecifications(ProductSpecParams productParams)
+                : base(x =>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))&&
+            (!productParams.BrandId.HasValue || x.Brandid == productParams.BrandId) &&
+            (!productParams.VehicleId.HasValue || x.Vehicleid == productParams.VehicleId) &&
+            (!productParams.SupplierId.HasValue || x.Supplierid == productParams.SupplierId) &&
+            (!productParams.PartCategoryId.HasValue || x.PartCategoryid == productParams.PartCategoryId)&&
+            (!productParams.PartNumerid.HasValue || x.PartNumberid== productParams.PartNumerid)
+            )
         {
             AddInclude(x => x.Brand);
             AddInclude(x => x.PartCategory);
@@ -17,6 +25,25 @@ namespace HansAfriqueApi.Specifications
             AddInclude(x => x.Vehicle);
             AddInclude(x => x.PartNumber);
             AddInclude(x => x.Photo);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1),
+                productParams.PageSize);
+
+            if (!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductSpecifications(int id): base(x =>x.id == id)
@@ -25,7 +52,7 @@ namespace HansAfriqueApi.Specifications
             AddInclude(x => x.PartCategory);
             AddInclude(x => x.Supplier);
             AddInclude(x => x.Vehicle);
-            AddInclude(x => x.PartNumber);
+            AddInclude(x => x.PartNumber); 
             AddInclude(x => x.Photo);
         }
     }
