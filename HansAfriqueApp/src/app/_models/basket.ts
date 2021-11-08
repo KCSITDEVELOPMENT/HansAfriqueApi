ac124ac1e5ed284917dcdbebaa8b6afced184efb
KCSITDEVELOPMENT <development@kcsit.co.za>
2021-11-08 13:56:00 +0200

    basketService

---
+++ HansAfriqueApp/src/app/_models/basket.ts
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class IBasket {
+        id: string = "";
+        items: IBasketItem[] = [];
+}
+    
+    export  class IBasketItem {
+        id: number=0;
+        name: string = "";
+        brand: string = "";
+        vehicle: string = "";
+        supplier: string = "";
+        price: number = 0;
+        pictureULR: string = "";
+        partCategory: string = "";
+        partCode: string = "";
+        vehicleModel: string = "";
+        partNumber: string = "";
+    }
+
+     
+export class Basket implements IBasket {
+    id= uuidv4();
+    items: IBasketItem[] = [];
+}
