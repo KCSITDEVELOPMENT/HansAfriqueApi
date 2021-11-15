fb5442ac921bdd6602f224560babe74ed9a3fb32
KCSITDEVELOPMENT <89642513+KCSITDEVELOPMENT@users.noreply.github.com>
2021-11-15 16:59:01 +0200

    orders Backend

---
+++ HansAfriqueApi/Entities/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HansAfriqueApi.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string PersonId { get; set; }
+        public Person Person { get; set; }
+
+    }
+}
--- HansAfriqueApi/Startup.cs
+++ HansAfriqueApi/Startup.cs
@@ -103,7 +103,7 @@ namespace HansAfriqueApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-           //     endpoints.MapFallbackToController("Index","Fallback");
+                //endpoints.MapFallbackToController("Index","Fallback");
             });
         }
     }
